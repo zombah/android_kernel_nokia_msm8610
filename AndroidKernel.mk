a346f8d7aa8630bc8944904da5230645eafcaf86
zombah <zombah@gmail.com>
2015-05-12 12:46:02 +0300

    Revert "[NOKIA-MSM8610] ENO soft and SBL LOG virtual framework support"
    This reverts commit 64bf0dac1ff6d50d406f908b4376dc3898efb45a.

--- AndroidKernel.mk
+++ AndroidKernel.mk
@@ -76,15 +76,7 @@ $(KERNEL_OUT):
 	mkdir -p $(KERNEL_OUT)
 
 $(KERNEL_CONFIG): $(KERNEL_OUT)
-	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm \
-		CROSS_COMPILE=arm-eabi- $(KERNEL_DEFCONFIG)
-ifneq ($(ENOSW_BUILD),)
-	@echo "Converting $(KERNEL_DEFCONFIG) for enosw"
-	echo 'CONFIG_ENO_SOFT=y' >> $@
-	echo 'CONFIG_SBL_LOG=y' >> $@
-	yes "" | $(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm \
-		CROSS_COMPILE=arm-eabi- oldconfig
-endif
+	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- $(KERNEL_DEFCONFIG)
 
 $(KERNEL_OUT)/piggy : $(TARGET_PREBUILT_INT_KERNEL)
 	$(hide) gunzip -c $(KERNEL_OUT)/arch/arm/boot/compressed/piggy.gzip > $(KERNEL_OUT)/piggy
